a0773341cb37c99b03bc8a8c198ae1ed9e86d469
Benjamin Perry <perbe24p@uoc02ft06spn78.staff.uod.otago.ac.nz>
2021-12-07 11:34:59 +1300

    Chapter 3.R updates all states

--- analysis/Chapter 3.R
+++ analysis/Chapter 3.R
@@ -1,13 +1,12 @@
-### Code for chapter 3. In Vitro Tn-seq
+### Code for chapter 3. In vitro Tn-seq
 
 setwd("~/Desktop/PhD/")
 set.seed(seed = 1953)
 
 library(tidyverse)
 
-
-
 readTradis <- function(wrkingdir = "") {
+  cwd<-getwd()
   setwd(dir = wrkingdir)
   tradisFiles <-
     list.files(pattern = ".tradis_gene_insert_sites.csv.all.csv")
@@ -24,17 +23,204 @@ readTradis <- function(wrkingdir = "") {
     tradisTable %>% select(locus_tag, ins_index, library)
   tradisTable <-
     tradisTable %>% pivot_wider(values_from = ins_index, names_from = library, values_fill = NA)
+  setwd(cwd)
   return(tradisTable)
 }
 
-# 1. Compile insertion index summary tables for analysis.
+### 1. Compile insertion index summary tables for analysis. ###
 
 ES <- readTradis(wrkingdir = "~/Desktop/PhD/data/tradis/essential/")
 INVITRO <- readTradis(wrkingdir = "~/Desktop/PhD/data/tradis/in vitro/")
 
+### 2. Compile Annotations Table ###
+# COGS <- read_tsv(file = "data/ref/R7A2020.eggNOG.tsv", col_names = F, trim_ws = T, comment = "#")
+# COGS <- COGS %>% rename(locus_tag = X1, gene = X6, COG = X21, CA = X22)
+# COGS <- COGS %>% select(locus_tag, gene, COG, CA)
+# KEGG <- read_table(file = "data/ref/R7A2020.KO.KofamKOALA.txt", col_names = F, comment = "#")
+# KEGG <- KEGG %>% rename(locus_tag = X2, KO = X3, KA = X7)
+# #Filter duplicate KEGG entries for smallest E-value
+# KEGG <- KEGG[order(KEGG$locus_tag, abs(KEGG$X6) ), ] ### sort first
+# KEGG <- KEGG[ !duplicated(KEGG$locus_tag), ]  ### Keep highest
+# KEGG <- KEGG %>%  select(locus_tag, KO, KA)
+# ANNOT <- left_join(COGS,KEGG)
 
+### 2.1 Take Mannual Annotations from Old Analysis ###
+# NOTE: At this point the annotation table (ANNOT) was mannual curated. A Column "FUN" was created which
+#       selected the single best COG category for genes of interest (ES/GD/etc.) but not all neutral genes.
+#       Annotations were included for non protein features such as tRNAs as well. I've read in this table below.
 
-# 2. 
+ANNOT <- read_csv(file = "data/ref/MLR7A_2020.annotations.csv")
+rm(COGS)
+rm(KEGG)
 
+### 3. Update the traDIS insertion index summary tables with essentiality states based on the thresholds calculated from the traDIS analysis. ###
+###    Essentiality States are for combined data of the WT and the NS datasets
+###    These values are printed out into a pdf file generate by the traDIS analysis pipeline. We then apply from rules to these thresholds which 
+###    increase the robustnesss of the states. Specicially, we select a decreased threshold for ICE genes due to decreased ins density, and we apply
+###    multipliers to the thresholds to identify cuts offs for GD genes.
 
+# Update the ins_index table to have NS ICE genes registered as NA
+ES <- left_join(ANNOT, ES)
+ES <- ES %>% rename(NS_ii = NS, WT_ii = WT)
+ES <- ES %>% mutate(NS_ii = replace(NS_ii, ICESym, NA))
 
+# Create state variable to hold the gene state, default is NE
+ES$ES_state <- "NE"
+
+ES$ES_state <- replace(x = ES$ES_state,
+                    ES$NS_ii < 0.05 & ES$WT_ii < 0.048,
+                    "CORE-ES")
+ES$ES_state <- replace(x = ES$ES_state,
+                    ES$NS_ii < 0.05 & ES$WT_ii > (0.048*2),
+                    "NS-ES")
+ES$ES_state <- replace(x = ES$ES_state,
+                    ES$NS_ii > (0.05*2) & ES$WT_ii < 0.048,
+                    "WT-ES")
+ES$ES_state <- replace(x = ES$ES_state,
+                    ES$NS_ii < 0.05 & ES$WT_ii <= (0.048*2) & ES$WT_ii >= 0.048,
+                    "GD")
+ES$ES_state <- replace(x = ES$ES_state,
+                    ES$NS_ii <= (0.05*2) & ES$NS_ii >= 0.05 & ES$WT_ii < 0.048 ,
+                    "GD")
+ES$ES_state <- replace(x = ES$ES_state,
+                    ES$NS_ii <= (0.05*2) & ES$NS_ii >= 0.05 & ES$WT_ii <= 0.001,
+                    "WT-ES")
+ES$ES_state <- replace(x = ES$ES_state,
+                    ES$NS_ii <= 0.001 & ES$WT_ii <= (0.048*2) & ES$WT_ii >= 0.048,
+                    "NS-ES")
+# Plot the correlated insertion indices
+ES %>% ggplot(aes(x=NS_ii, y=WT_ii, color=ES_state, alpha=0.1)) + 
+  geom_point() + 
+  theme_minimal(base_size = 20) + 
+  guides(colour = guide_legend(override.aes = list(size=8))) +
+  geom_hline(yintercept = 0.048) +
+  geom_vline(xintercept = 0.05)
+ggsave("results/chapter 3 in vitro/ES.ins_index.png", width = 12, height = 8)
+
+ES %>% ggplot(aes(x=NS_ii, y=WT_ii, color=ES_state, alpha=0.1)) + 
+  geom_point() + 
+  theme_minimal(base_size = 20) + 
+  guides(colour = guide_legend(override.aes = list(size=8))) +
+  geom_hline(yintercept = 0.048) +
+  geom_vline(xintercept = 0.05) + xlim(0,0.175) + ylim(0,0.175)
+ggsave("results/chapter 3 in vitro/ES.ins_index.zoom.png", width = 12, height = 8)
+
+### 4. Update the traDIS insertion index summary tables for the conditionally essential states using similar rules to section 3. ###
+
+# Update the insertion index table to have NS ICE genes registered as NA
+INVITRO <- left_join(ANNOT, INVITRO)
+INVITRO <- INVITRO %>% rename(NSTB_ii = `NS-INPUT`, NSGR_ii = NSGR, NSTY_ii = NSTY,
+                              WTTB_ii = `WT-INPUT`, WTGR_ii = WTGR, WTTY_ii = WTTY)
+INVITRO <- INVITRO %>% mutate(NSTB_ii = replace(NSTB_ii, ICESym, NA),NSGR_ii = replace(NSGR_ii, ICESym, NA),NSTY_ii = replace(NSTY_ii, ICESym, NA))
+
+###    TY States - TY_states
+INVITRO$TY_state <- "NE"
+INVITRO$TY_state <- replace(x = INVITRO$TY_state,
+                    INVITRO$NSTY_ii < 0.016 & INVITRO$WTTY_ii < 0.018,
+                    "TY-ES")
+INVITRO$TY_state <- replace(x = INVITRO$TY_state,
+                    INVITRO$NSTY_ii < 0.016 & INVITRO$WTTY_ii > (0.018*2),
+                    "NS-TY-ES")
+INVITRO$TY_state <- replace(x = INVITRO$TY_state,
+                    INVITRO$NSTY_ii > (0.016*2) & INVITRO$WTTY_ii < 0.018,
+                    "WT-TY-ES")
+
+INVITRO$TY_state <- replace(x = INVITRO$TY_state,
+                    INVITRO$NSTY_ii < 0.016 & INVITRO$WTTY_ii <= (0.018*2) & INVITRO$WTTY_ii >= 0.018,
+                    "TY-GD")
+INVITRO$TY_state <- replace(x = INVITRO$TY_state,
+                    INVITRO$NSTY_ii <= (0.016*2) & INVITRO$NSTY_ii >= 0.016 & INVITRO$WTTY_ii < 0.018,
+                    "TY-GD")
+
+INVITRO$TY_state <- replace(x = INVITRO$TY_state,
+                    INVITRO$NSTY_ii <= 0.001 & INVITRO$WTTY_ii <= (0.018*2) & INVITRO$WTTY_ii >= 0.018,
+                    "NS-TY-ES")
+INVITRO$TY_state <- replace(x = INVITRO$TY_state,
+                    INVITRO$NSTY_ii <= (0.016*2) & INVITRO$NSTY_ii >= 0.016 & INVITRO$WTTY_ii <= 0.001,
+                    "WT-TY-ES")
+
+INVITRO <- INVITRO %>% mutate(TY_state = as.factor(TY_state))
+INVITRO$TY_state <- factor(INVITRO$TY_state, levels = c("TY-ES", "TY-GD", "NE", "NS-TY-ES", "WT-TY-ES"))
+
+INVITRO %>% ggplot(aes(x=NSTY_ii, y=WTTY_ii, color=TY_state, alpha=0.1)) + 
+  geom_point() + 
+  theme_minimal(base_size = 20) + 
+  guides(colour = guide_legend(override.aes = list(size=8))) +
+  geom_hline(yintercept = 0.018) +
+  geom_vline(xintercept = 0.016) + xlim(0,0.175) + ylim(0,0.175)
+ggsave("results/chapter 3 in vitro/TY.ins_index.png", width = 12, height = 8)
+
+###    GR States - GR_states
+INVITRO$GR_state <- "NE"
+INVITRO$GR_state <- replace(x = INVITRO$GR_state,
+                    INVITRO$NSGR_ii < 0.015 & INVITRO$WTGR_ii < 0.012,
+                    "GR-ES")
+INVITRO$GR_state <- replace(x = INVITRO$GR_state,
+                    INVITRO$NSGR_ii < 0.015 & INVITRO$WTGR_ii > (0.012*2),
+                    "NS-GR-ES")
+INVITRO$GR_state <- replace(x = INVITRO$GR_state,
+                    INVITRO$NSGR_ii > (0.015*2) & INVITRO$WTGR_ii < 0.012,
+                    "WT-GR-ES")
+
+INVITRO$GR_state <- replace(x = INVITRO$GR_state,
+                    INVITRO$NSGR_ii < 0.015 & INVITRO$WTGR_ii <= (0.012*2) & INVITRO$WTGR_ii >= 0.012,
+                    "GR-GD")
+INVITRO$GR_state <- replace(x = INVITRO$GR_state,
+                    INVITRO$NSGR_ii <= (0.015*2) & INVITRO$NSGR_ii >= 0.015 & INVITRO$WTGR_ii < 0.012,
+                    "GR-GD")
+INVITRO$GR_state <- replace(x = INVITRO$GR_state,
+                    INVITRO$NSGR_ii <= 0.001 & INVITRO$WTGR_ii <= (0.012*2) & INVITRO$WTGR_ii >= 0.012,
+                    "NS-GR-ES")
+INVITRO$GR_state <- replace(x = INVITRO$GR_state,
+                    INVITRO$NSGR_ii <= (0.015*2) & INVITRO$NSGR_ii >= 0.015 & INVITRO$WTGR_ii <= 0.001,
+                    "WT-GR-ES")
+
+INVITRO <- INVITRO %>% mutate(GR_state = as.factor(GR_state))
+INVITRO$GR_state <- factor(INVITRO$GR_state, levels = c("GR-ES", "GR-GD", "NE", "NS-GR-ES", "WT-GR-ES"))
+
+INVITRO %>% ggplot(aes(x=NSGR_ii, y=WTGR_ii, color=GR_state, alpha=0.1)) + 
+  geom_point() + 
+  theme_minimal(base_size = 20) + 
+  guides(colour = guide_legend(override.aes = list(size=8))) +
+  geom_hline(yintercept = 0.012) +
+  geom_vline(xintercept = 0.015) + xlim(0,0.175) + ylim(0,0.175)
+
+ggsave("results/chapter 3 in vitro/GR.ins_index.png", width = 12, height = 8)
+
+###    TB States - TB_states
+INVITRO$TB_state <- "NE"
+INVITRO$TB_state <- replace(x = INVITRO$TB_state,
+                    INVITRO$NSTB_ii < 0.017 & INVITRO$WTTB_ii < 0.015,
+                    "TB-ES")
+INVITRO$TB_state <- replace(x = INVITRO$TB_state,
+                    INVITRO$NSTB_ii < 0.017 & INVITRO$WTTB_ii > (0.015*2),
+                    "NS-TB-ES")
+INVITRO$TB_state <- replace(x = INVITRO$TB_state,
+                    INVITRO$NSTB_ii > (0.017*2) & INVITRO$WTTB_ii < 0.015,
+                    "WT-TB-ES")
+
+INVITRO$TB_state <- replace(x = INVITRO$TB_state,
+                    INVITRO$NSTB_ii < 0.017 & INVITRO$WTTB_ii <= (0.015*2) & INVITRO$WTTB_ii >= 0.015,
+                    "TB-GD")
+INVITRO$TB_state <- replace(x = INVITRO$TB_state,
+                    INVITRO$NSTB_ii <= (0.017*2) & INVITRO$NSTB_ii >= 0.017 & INVITRO$WTTB_ii < 0.015,
+                    "TB-GD")
+
+INVITRO$TB_state <- replace(x = INVITRO$TB_state,
+                    INVITRO$NSTB_ii <= 0.001 & INVITRO$WTTB_ii <= (0.015*2) & INVITRO$WTTB_ii >= 0.015,
+                    "NS-TB-ES")
+INVITRO$TB_state <- replace(x = INVITRO$TB_state,
+                    INVITRO$NSTB_ii <= (0.017*2) & INVITRO$NSTB_ii >= 0.017 & INVITRO$WTTB_ii <= 0.001,
+                    "WT-TB-ES")
+
+INVITRO <- INVITRO %>% mutate(TB_state = as.factor(TB_state))
+INVITRO$TB_state <- factor(INVITRO$TB_state, levels = c("TB-ES", "TB-GD", "NE", "NS-TB-ES", "WT-TB-ES"))
+
+INVITRO %>% ggplot(aes(x=NSTB_ii, y=WTTB_ii, color=TB_state, alpha=0.1)) + 
+  geom_point() + 
+  theme_minimal(base_size = 20) + 
+  guides(colour = guide_legend(override.aes = list(size=8))) +
+  geom_hline(yintercept = 0.015) +
+  geom_vline(xintercept = 0.017) + xlim(0,0.175) + ylim(0,0.175)
+
+ggsave("results/chapter 3 in vitro/TB.ins_index.png", width = 12, height = 8)
